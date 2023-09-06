f17d16a164864e61827217edd343a0f8182ec0f4
Stanislav <amirov202@gmail.com>
2023-09-06 13:49:19 +0300

    blog

--- app/js/main.js
+++ app/js/main.js
@@ -22,6 +22,11 @@ $(function () {
         dots: true,
         arrows: false,
     });
+    // слайдер из странички blog
+    $('.article-slider__box').slick({
+        prevArrow: '<button type="button" class="article-slider__arrow article-slider__arrowleft"><img src="images/arrow-slide-left.svg" alt="arrow left"></button>',
+        nextArrow: '<button type="button" class="article-slider__arrow article-slider__arrowright"><img src="images/arrow-slide-right.svg" alt="arrow right"></button>'
+    });
     // mixitup
     var mixer = mixitup('.gallery__inner', {
         load: {
